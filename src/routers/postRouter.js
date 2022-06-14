83945df0ca59d6ac10b70bc82912f12c623a01f5
matheusfacciolla <matheustassifacciolla@gmail.com>
2022-06-14 15:48:35 -0300

    feat: add curtidas and edit/delete endpoint

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -2,10 +2,15 @@ import { Router } from "express";
 
 import {validateSchema} from "../middlewares/schemaValidator.js"
 import postSchema from "../schemas/postSchema.js"
-import publishPost from "../controllers/postController.js";
+import {publishPost, likePost, deslikePost, countLikes, deletePost, editPost } from "../controllers/postController.js";
 
 const postRouter = Router()
 
 postRouter.post("/publish/post", validateSchema(postSchema), publishPost);
+postRouter.post("/like/post", likePost)
+postRouter.delete("/deslike/post", deslikePost)
+postRouter.get("/coutlikes/post", countLikes)
+postRouter.delete("/delete/post", deletePost)
+postRouter.post("/edit/post", editPost)
 
 export default postRouter;
