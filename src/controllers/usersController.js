3755dd7a1d53a6336498eca462e933e885a31492
Jorge Tadeu Soares Batalha <jsbatalha@gmail.com>
2022-06-19 12:51:34 -0300

    adicionando pesquisa de usuário por id

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,16 +3,18 @@ import connection from "../config/db.js";
 export async function getUsers(req,res){
     const {name, id} = req.query
 
-    const nameClause = name? 
+    const nameClause = name? `users.name ILIKE ${name}` : ``;
+    const idClause = id ? `users.id = ${id}` : ``;
 
     try{
         const users = await connection.query(
             `
             SELECT users.name AS name, users.id AS id, users.image AS image
             FROM users
-            WHERE users.name ILIKE $1
-            `,
-            [name + "%"]
+            WHERE 
+            ${nameClause}
+            ${idClause}
+            `
         );
 
         res.status(200).send(users.rows);
