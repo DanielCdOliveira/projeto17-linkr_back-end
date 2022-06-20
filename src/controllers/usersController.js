0fa61ec3a9176a8b0098d380d0f5a3a033451aab
Jorge Tadeu <jsbatalha@gmail.com>
2022-06-20 19:52:03 -0300

    Creating usersRepository

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,26 +1,16 @@
-import connection from "../config/db.js";
+import usersRepository from "../repositories/usersRepository.js";
 
-export async function getUsers(req,res){
+export async function getUser(req,res){
     const {name, id} = req.query
 
-    const nameClause = name? `users.name ILIKE ${name}` : ``;
-    const idClause = id ? `users.id = ${id}` : ``;
-
     try{
-        const users = await connection.query(
-            `
-            SELECT users.name AS name, users.id AS id, users.image AS image
-            FROM users
-            WHERE 
-            ${nameClause}
-            ${idClause}
-            `
-        );
+        const users = await usersRepository.findUser(name, id);
 
         res.status(200).send(users.rows);
     }
     catch(err){
-        res.status(500).send("Erro no servidor");
+        console.log("Erro ao buscar usuário");
+        console.log(err);
+        res.status(500).send(err);
     }
 }
-
---
+++ src/repositories/usersRepository.js
@@ -0,0 +1,25 @@
+import connection from "../config/db.js";
+
+async function findUser(name, id){
+
+    const nameClause = name? `users.name ILIKE ${name}` : ``;
+    const idClause = id ? `users.id = ${id}` : ``;
+
+    return(
+        await connection.query(
+        `
+        SELECT users.name AS name, users.id AS id, users.image AS image
+        FROM users
+        WHERE 
+        ${nameClause}
+        ${idClause}
+        `
+        )
+    )
+}
+
+const usersRepository = {
+    findUser
+}
+
+export default usersRepository
